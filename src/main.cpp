5dd9e28e04a8d5f615df52e511cc62c177dc5e89
Zhitao Gong <zhitaao.gong@gmail.com>
2019-09-23 17:53:38 -0400

    support for multi-monitor

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,48 @@
 #include <QApplication>
 #include <QCommandLineParser>
+#include <QScreen>
 #include <QWindow>
-#include <iostream>
 #include <vector>
 
 #include "widget.h"
 
 int main(int argc, char *argv[])
 {
-  QApplication app(argc, argv);
-  QCoreApplication::setApplicationName("block-qt");
-  QCoreApplication::setApplicationVersion("1.1");
+    QApplication app(argc, argv);
+    QCoreApplication::setApplicationName("block-qt");
+    QCoreApplication::setApplicationVersion("1.1");
 
-  QCommandLineParser parser;
+    QCommandLineParser parser;
 
-  QCommandLineOption durationOption(
-      QStringList() << "d" << "duration",
-      QCoreApplication::translate("main", "Shadows the screen for <x> minutes"),
-      QCoreApplication::translate("main", "x"));
-  parser.addOption(durationOption);
+    QCommandLineOption durationOption(
+        QStringList() << "d" << "duration",
+        QCoreApplication::translate("main", "Shadows the screen for <x> minutes"),
+        QCoreApplication::translate("main", "x"));
+    parser.addOption(durationOption);
 
-  parser.setApplicationDescription(
-      "\nShadows the screen for a while so that you could take a"
-      "break.\nThis program does NOT invoke itself, I used it along "
-      "with `cron` program.");
-  parser.addHelpOption();
-  parser.addVersionOption();
+    parser.setApplicationDescription(
+                "\nShadows the screen for a while so that you could take a"
+                "break.\nThis program does NOT invoke itself, I used it along "
+                "with `cron` program.");
+    parser.addHelpOption();
+    parser.addVersionOption();
 
-  parser.process(app);
+    parser.process(app);
 
-  int duration = parser.value(durationOption).toInt();
-  if (!duration) duration = 5;
+    int duration = parser.value(durationOption).toInt();
+    if (!duration) duration = 5;
 
-  auto all_screens = app.screens();
-  std::vector<Widget *> widgets;
-  for (int i = 0; i < all_screens.size(); ++i) {
-    Widget *w = new Widget(duration);
-    w->show();
-    w->windowHandle()->setScreen(all_screens[i]);
-    widgets.push_back(w);
-  }
+    std::vector<Widget *> widgets;
+    for (QScreen* screen : app.screens()) {
+        Widget *w = new Widget(duration);
+        QRect rect = screen->geometry();
+        w->move(rect.center().x() - w->width() / 2,
+                rect.center().y() - w->height() / 2);
+        w->show();
+        widgets.push_back(w);
+    }
 
-  int retcode = app.exec();
-  for (Widget *w : widgets) delete w;
-  return retcode;
+    int retcode = app.exec();
+    for (Widget *w : widgets) delete w;
+    return retcode;
 }
